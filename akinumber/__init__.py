3af0d967c808b63744b414f79b622f7029f694fc
HarryWhen <h59949952@gmail.com>
2025-03-17 01:36:05 +0500

    Add 'game' module

--- akinumber/__init__.py
+++ akinumber/__init__.py
@@ -2,12 +2,13 @@ from flask import Flask
 
 from utils import get_valid_name
 
-from . import db
+from . import db, game
 
 
 def create_app():
     app = Flask(get_valid_name(__loader__.name))
 
     db.init_app(app)
+    app.register_blueprint(game.bp)
 
     return app
